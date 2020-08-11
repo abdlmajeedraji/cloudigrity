84f2b579d21c3bf364bfdf58a7fe375fe8ffaba8
Abdulmajeed A. Raji <45627583+atoyebii@users.noreply.github.com>
2020-08-11 08:52:50 +0200

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect("den1.mysql2.gear.host","cloudigrity","Kg09FDTkT~~n","cloudigrity");
+$con = mysqli_connect("den1.mysql1.gear.host","cloudigrity","Kg09FDTkT~~n","cloudigrity");
 // Check connection
 if (mysqli_connect_errno())
   {
